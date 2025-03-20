38f732c33e6db1b571c10dd87e216b9cef55d491
Ifeoluwa Odewale <ifeoluwa.odewale@gmail.com>
2025-03-20 22:46:48 +0100

    Remove type any

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getSession } from "@/lib/sessions/actions";
 
 export async function GET() {
- const session: any = await getSession();
+ const session = await getSession();
 
  return NextResponse.json(session);
 }
