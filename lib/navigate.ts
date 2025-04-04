84957bbfb7ab91ae94a3bf086c1212eb9b141c91
ryanfavour4 <ryanfave022@gmail.com>
2025-04-04 15:54:21 +0100

    Remove unused imports from navigation utility

--- lib/navigate.ts
+++ lib/navigate.ts
@@ -1,6 +1,5 @@
 // lib/navigation.ts
 "use client";
-import { redirect, useRouter } from "next/navigation";
 
 export const goTo = (path: string = "/auth/login") => {
     if (typeof window !== "undefined") {
